cf3ed2d676dcddbaabe476312932c52792ee1b2e
KeytacK <t.sekky0815@gmail.com>
2018-07-23 19:28:23 +0900

    [update] インデント制御

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ browser.runtime.onMessage.addListener( (message) => {
 	var select = window.getSelection();
 
 	var code = document.createElement('code');
-	code.textContent = select.toString();
+	code.textContent = indent(select.toString());
 
 	var insElm = document.createElement('pre');
 	insElm.innerHTML = code.outerHTML;
@@ -11,3 +11,35 @@ browser.runtime.onMessage.addListener( (message) => {
 	range.deleteContents();
 	range.insertNode(insElm);
 });
+
+var indent = str => {
+	var depth = 0;
+	var rows = str.split(/\r\n/);
+
+	var ret = "";
+	for (const row of rows) {
+
+		if (row.match(/\{$/)) {
+			ret += getIndent(depth) + row + "\r\n";
+			depth++;
+		} else if (row.match(/^\}/)) {
+			depth--;
+			ret += getIndent(depth) + row + "\r\n";
+		} else {
+			ret += getIndent(depth) + row + "\r\n";
+		}
+
+	}
+
+	return ret;
+};
+
+const INDENT_UNIT = "  ";
+var getIndent = depth => {
+	var indent = "";
+	for (let i = 0; i <= depth; ++i) {
+		indent += INDENT_UNIT;
+	}
+
+	return indent;
+};
