ebf957f912c3b332b8f4ca7c5d3cd74fdfc903c2
KeytacK <t.sekky0815@gmail.com>
2018-07-23 11:05:40 +0900

    [update] editorconfig設定変更

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 browser.runtime.onMessage.addListener( (message) => {
-    var select = window.getSelection();
+	var select = window.getSelection();
 
-    var insElm = document.createElement('pre');
-    insElm.innerHTML = '<code>' + select.toString() + '</code>';
+	var insElm = document.createElement('pre');
+	insElm.innerHTML = '<code>' + select.toString() + '</code>';
 
-    var range = select.getRangeAt(0);
-    range.deleteContents();
-    range.insertNode(insElm);
+	var range = select.getRangeAt(0);
+	range.deleteContents();
+	range.insertNode(insElm);
 });
